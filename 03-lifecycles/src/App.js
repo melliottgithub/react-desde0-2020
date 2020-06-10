20b89eb710d801058e8579ecc59827f24c718be1
Michael <kllmmc23@gmail.com>
2020-06-10 11:10:01 -0600

    componentDidUpdate state

--- 03-lifecycles/src/App.js
+++ 03-lifecycles/src/App.js
@@ -5,22 +5,40 @@ import Header from "./components/Header";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { posts: [] };
+    this.state = {
+      id: 1,
+      post: [],
+    };
   }
 
   async componentDidMount() {
     // fetch("https://jsonplaceholder.typicode.com/posts")
     //   .then((res) => res.json())
     //   .then((data) => console.log(data));
-
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const { id } = this.state;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
     const data = await res.json();
-    this.setState({ posts: data });
+    this.setState({ post: data });
     window.addEventListener("scroll", () => console.log("SCROLL"));
   }
 
+  onClickHandler = () => {
+    this.setState({ id: this.state.id + 1 });
+  };
+
+  async componentDidUpdate(prevProps,prevState) {
+    if (prevState.id !== this.state.id) {
+       const { id } = this.state;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const data = await res.json();
+    this.setState({ post: data });
+      
+    }
+    
+  }
+
   render() {
-    const { posts } = this.state;
+    const { post } = this.state;
 
     if (true) {
       return (
@@ -28,12 +46,20 @@ class App extends Component {
           <header className="App-header">
             <Header title="Lifecycle" />
             <div>
-              {posts.map(({ title, body, id }) => (
+              {/* {posts.map(({ title, body, id }) => (
                 <Fragment key={id}>
                   <h2>{title}</h2>
                   <p>{body}</p>
                 </Fragment>
-              ))}
+              ))} */}
+              {
+                <Fragment key={this.state.id}>
+                  <button onClick={this.onClickHandler}>Next</button>
+                  <h2>Post con id: {post.id}</h2>
+                  <h2>{post.title}</h2>
+                  <p>{post.body}</p>
+                </Fragment>
+              }
             </div>{" "}
           </header>
         </div>
