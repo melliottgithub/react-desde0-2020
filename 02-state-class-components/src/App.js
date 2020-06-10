fa82f980275b2777d0cfd0d7f032e3eba684d1f1
Michael <kllmmc23@gmail.com>
2020-06-09 22:19:41 -0600

    exercise

--- 02-state-class-components/src/App.js
+++ 02-state-class-components/src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  // }
+
   state = { number: 5 };
+
+  handleIncrement = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  handleReset = () => {
+    this.setState({ number: 0 });
+  };
+
   render() {
     const { number, name } = this.props;
     return (
       <div className="App">
         <header className="App-header">
-          {name} {number}
+          Number: {this.state.number}
+          <button onClick={this.handleIncrement}>Increment</button>
+          <button onClick={this.handleDecrement}>Decrement</button>
+          <button onClick={this.handleReset}>Reset</button>
         </header>
       </div>
     );
--- 02-state-class-components/src/index.js
+++ 02-state-class-components/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App number={3} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
